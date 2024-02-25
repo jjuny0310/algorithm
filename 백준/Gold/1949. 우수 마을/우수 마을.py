3e5f8e833689c556abc7115532d8a41896ffedf0
이용준 <59381113+jjuny0310@users.noreply.github.com>
2024-02-25 12:40:16 +0900

    [Gold II] Title: 우수 마을, Time: 72 ms, Memory: 35756 KB -BaekjoonHub

--- 백준/Gold/1949. 우수 마을/우수 마을.py
+++ 백준/Gold/1949. 우수 마을/우수 마을.py
@@ -1,14 +1,13 @@
 import sys
+
 sys.setrecursionlimit(10 ** 6)
 input = sys.stdin.readline
 
 
 def dp_solve(now):
-    visited[now] = True
-    dp[now][0] = 0
-    dp[now][1] = citizen[now]
     for node in graph[now]:
         if not visited[node]:
+            visited[node] = True
             dp_solve(node)
             dp[now][0] += max(dp[node][0], dp[node][1])
             dp[now][1] += dp[node][0]
@@ -23,7 +22,8 @@ for _ in range(n - 1):
     graph[a].append(b)
     graph[b].append(a)
 
-dp = [[0] * 2 for _ in range(n + 1)]
+dp = [[0, citizen[i]] * 2 for i in range(n + 1)]
 visited = [False] * (n + 1)
+visited[1] = True
 dp_solve(1)
 print(max(dp[1]))
